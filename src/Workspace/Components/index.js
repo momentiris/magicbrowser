9816a1d263c8898dad8290fd37a03f8ce0b6c649
sebastianpalm <sebastian.palm91@gmail.com>
2018-11-07 13:05:11 +0100

    styling and some other changes

--- src/Workspace/Components/index.js
+++ src/Workspace/Components/index.js
@@ -2,8 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { createSelector } from 'reselect';
-import WsButton from './WorkSpaceButton/wsbutton';
-import WsHover from './WorkSpaceButton/wshover';
+// import WsButton from './WorkSpaceButton/wsbutton';
+// import WsHover from './WorkSpaceButton/wshover';
 import {
   Container,
   Column,
@@ -11,25 +11,37 @@ import {
   Li,
   ItemLink,
   Ua,
-  UaHeader,
   TabItems,
   TabWrapper,
   Button,
   Hover,
+  AddNewWs,
+  Input,
+  CreateButton,
+  CancelButton
 } from './styles';
 
 import {
   addWorkspace,
-  switchWorkspaces
+  switchWorkspaces,
+  initEmptyWorkspace,
 } from '../actions';
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     console.log(this.props.hover);
+    this.state = {
+      toggle: false,
+      workspacename: 'Change me'
+    };
   }
 
   componentDidMount(){
+  }
+
+  onToggle = () => {
+    this.setState({ toggle: !this.state.toggle });
   }
 
   switchWorkspaces = value => {
@@ -40,22 +52,52 @@ class Dashboard extends Component {
     this.switchWorkspaces(e.target.value);
   }
 
+  addWorkspace = e => {
+    e.preventDefault();
+    this.props.initEmptyWorkspace();
+    this.props.addWorkspace(this.state.workspacename);
+    this.setState({ toggle: !this.state.toggle });
+  }
+
+  handleInputChange = e => {
+    this.setState({ workspacename: e.target.value });
+  }
+
   // TODO: Move the Button and hover to own components, to make different states
   //       More styling
   //       fix the flex-wrap correct
 
   render() {
     const tabs = this.props.tabs;
-    const workspaces = this.props.workspaces;
+    const workspaces = [].concat(this.props.workspaces).sort((a, b) => a.item - b.item);
     return (
       <Container>
-        <Ua>
-          <UaHeader>Back</UaHeader>
-          <UaHeader>New space</UaHeader>
-        </Ua>
+        <AddNewWs>
+          <Button onClick={this.onToggle}>New space</Button>
+          <br />
+          {
+            this.state.toggle ?
+              <form onSubmit={this.addWorkspace}>
+                <Hover />
+                <Input onChange={this.handleInputChange} type="text" placeholder="Name your workspace"/>
+                <br />
+                <CreateButton type="submit">Create</CreateButton>
+                <CancelButton>Cancel</CancelButton>
+              </form>
+              : null
+          }
+        </AddNewWs>
         <Column>
           <Ul name="workspaces">
-            { workspaces.map((ws, i) => <Li key={i}> <Button onClick={this.handleClick} value={ws}> <Hover></Hover> {ws}</Button></Li> )}
+            {
+              workspaces.map((ws, i) =>
+                <Li key={i}>
+                  <Button onClick={this.handleClick} value={ws}>
+                    <Hover />
+                    {ws}
+                  </Button>
+                </Li>
+              )}
           </Ul>
           <TabWrapper >
             { tabs.map((tab, i) => <TabItems id={i} key={i}> {tab.src} </TabItems> )}
@@ -83,6 +125,7 @@ const mapActionsToProps = (dispatch, props) => {
   return bindActionCreators({
     switchWorkspaces: switchWorkspaces,
     addWorkspace: addWorkspace,
+    initEmptyWorkspace: initEmptyWorkspace,
   }, dispatch);
 };
 
@@ -101,6 +144,9 @@ const mergeProps = (stateProps, dispatchProps, ownProps) => {
     tabs: stateProps.workspaces[stateProps.workspaces.current].tabs,
     workspaces: withoutCurrent,
     switchWorkspaces: arg => dispatchProps.switchWorkspaces(arg),
+    addWorkspace: arg => dispatchProps.addWorkspace(arg),
+    initEmptyWorkspace: () => dispatchProps.initEmptyWorkspace(),
+    renameWorkspace: arg => dispatchProps.renameWorkspace(arg),
   });
 };
 
--- src/common/stylesheet.js
+++ src/common/stylesheet.js
@@ -1,5 +1,17 @@
 export const stylesheet = {
-  darkgrey: '#C4C4C4',
-  mediumgrey: '#EAEAEA',
-  lightgrey: '#F5F5F5',
+  // Colors
+  darkgrey: '#CDD0D6',
+  mediumgrey: '#DDDFE3',
+  lightgrey: '#E9EBEE',
+  lightergrey: '#F5F5F5',
+  bluepurple: '#4738F9',
+  white: '#FFFFFF',
+  black: '#000000',
+  // Font sizes
+  small: '12px',
+  medium: '16px',
+  large: '24px',
+  // Button config
+  paddingbutton: '6px 22px',
+  radius4: '4px'
 };
